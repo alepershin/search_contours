4167ea98536c066cc11c039c5b7413a477af57ec
alepershin <alepershin@mail.ru>
2024-04-26 18:23:33 +0400

    Added recognition

--- main.py
+++ main.py
@@ -2,6 +2,40 @@ import cv2
 import numpy as np
 import streamlit as st
 
+from keras.models import load_model
+
+# Загрузка моделей
+model_28_28 = load_model('model_28_28.h5')
+model_56_28 = load_model('model_56_28.h5')
+model_112_28 = load_model('model_112_28.h5')
+
+def classify_contour(contour, image):
+    x, y, w, h = cv2.boundingRect(contour)
+    cropped_image = image[y:y+h, x:x+w]
+    ratio = w / h
+
+    # Масштабирование изображения до размеров, подходящих для модели
+    if ratio < 2:
+        resized_image = cv2.resize(cropped_image, (28, 28), interpolation=cv2.INTER_AREA)
+        model = model_28_28
+    elif ratio < 4:
+        resized_image = cv2.resize(cropped_image, (56, 28), interpolation=cv2.INTER_AREA)
+        model = model_56_28
+    else:
+        resized_image = cv2.resize(cropped_image, (112, 28), interpolation=cv2.INTER_AREA)
+        model = model_112_28
+
+    # Нормализация изображения
+    resized_image = resized_image / 255.0
+    resized_image = resized_image.reshape(1, resized_image.shape[0], resized_image.shape[1], 1)
+
+    # Предсказание
+    prediction = model.predict(resized_image)
+    # Верните идентификатор класса, категорию или символ в зависимости от того, как обучена модель
+    # Тем временем предположим, что она возвращает идентификатор класса
+    class_id = np.argmax(prediction)
+    return class_id
+
 def preprocess_image(image, threshold):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, binary = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY_INV)
@@ -18,6 +52,11 @@ def draw_rectangles(image, contours, min_contour_area, max_contour_area):
         if min_contour_area < area < max_contour_area:
             x, y, w, h = cv2.boundingRect(contour)
             cv2.rectangle(result, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            class_id = classify_contour(contour, image)
+            # Обеспечим, чтобы текст не выходил за верхнюю границу изображения
+            text_y = y - 10 if y - 10 > 10 else y + h + 20
+            # Выводим текст красным цветом для лучшей видимости
+            cv2.putText(result, str(class_id), (x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
     return result
 
 st.title("Обнаружение контуров рукописных символов")
